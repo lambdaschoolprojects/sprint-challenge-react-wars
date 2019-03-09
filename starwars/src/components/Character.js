abb1b048ab39c87ef4aa8695ede4a31e4d058d68
Rane Wallin <RAN2143066@maricopa.edu>
2019-03-08 19:45:49 -0700

    Characters rendered to screen, no styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
-const Character = ({
+const Character =  ({ characterData: {
     birth_year,
     eye_color,
-    films,
     hair_color,
     height,
     name,
     mass,
-    skin_color,
-                   }) => {
+    skin_color
+}})  => {
+
     return (
       <div className="character">
           <h2>{ name }</h2>
+          <div className="characterData">
+              <p><strong>Birth Year</strong> {birth_year}</p>
+              <p><strong>Eye Color</strong>{eye_color}</p>
+              <p><strong>Hair Color</strong>{hair_color}</p>
+              <p><strong>Height</strong>{height}</p>
+              <p><strong>Weight</strong>{mass}</p>
+              <p><strong>Skin Color</strong>{skin_color}</p>
+          </div>
       </div>
     );
 }
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Character from "./Character";
 
-const CharacterList = props => {
+const CharacterList = ({ characterList }) => {
     return (
-        <div><Character/></div>
+        <div>
+            { renderCharacter(characterList) }
+        </div>
     )
+}
+
+const renderCharacter = characterList => {
+    const characters = characterList.map(character => <Character characterData={character} />);
+
+
+    return characters;
 }
 
 export default CharacterList;
